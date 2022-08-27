fa64460cea885035a51c349514908f18a5c36da1
Hesham-Youssef <hesham_ahmed2002002@yahoo.com>
2022-08-27 05:22:15 +0200

    create method
    improvements

--- schema.py
+++ schema.py
@@ -25,27 +25,18 @@ def parse_args():
 
 
 
-def creat(schema):
-    xs = [None] * 3
-    i=0
-    with open('database.py','r') as file:
-        say1 = 'database_name'
-        say2 = 'name'
-        for line in file:
-            for word in line.split():
-                if say1 == word :
-                    plit1 = line.split().index(say1)
-                    after1 = line.split()[5:6]
-                if say2 == word : 
-                    plit2 = line.split().index(say2)
-                    after2 = line.split()[5:6]
-                    xs[i]=after2
-                    i=i+1
-    os.mkdir(after1[0])
-    root_path = os.path.realpath(after1[0])
-    for i in range(3):
-        path = os.path.join(root_path, xs[i][0])
-        os.mkdir(path)
+def create(schema):
+    with open('database.json','r') as database:
+        data = json.load(database)
+        if(os.path.exists(data[" database_name "])):
+            return
+        os.mkdir(data[" database_name "])
+        root_path = os.path.realpath(data[" database_name "])
+        for x in data[' Tables ']:
+            path = os.path.join(root_path, x[' name '])
+            os.mkdir(path)
+         
 
 args = parse_args()
-creat(args.schema)
+
+create(args.schema)
---
+++ test.py
@@ -0,0 +1,16 @@
+import unittest
+
+class databaseTest(unittest.TestCase):
+    
+    def test1(self):
+        self.assertEqual(first, second)
+        
+    def test2(self):
+        self.assertEqual(first, second)
+        
+    def test3(self):
+        self.assertEqual(first, second)
+        
+        
+if __name__ == '__main__':
+    unittest.main()
