76743bc95e0265298c7081ccb5287f209ed4030d
NancyHisham <96303049+NancyHisham@users.noreply.github.com>
2022-08-29 19:21:27 +0200

    edit CommandFactory

--- schema.py
+++ schema.py
@@ -8,8 +8,16 @@ import os
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Auto Sizer agent")
-
     parser.add_argument(
+<<<<<<< Updated upstream
+=======
+        "-cmd",
+        "--command",
+        type=str,
+        help= "Command : Create , Get , Set , Delete",
+    )
+    parser.add_argument(
+>>>>>>> Stashed changes
         "-pk",
         "--primary-key",
         type=str,
@@ -21,6 +29,24 @@ def parse_args():
         type=str,
         help="This is the database schema, it is a json object.",
     )
+    parser.add_argument(
+        "-db",
+        "--database",
+        type=str,
+        help="The Database name",
+    )
+    parser.add_argument(
+        "-t",
+        "--table",
+        type=str,
+        help="The Table name",
+    )
+    parser.add_argument(
+        "-v",
+        "--value",
+        type=str,
+        help="The Value to add",
+    )
     return parser.parse_args()
 
 
@@ -28,15 +54,18 @@ def parse_args():
 def create(schema):
     with open('database.json','r') as database:
         data = json.load(database)
-        if(os.path.exists(data[" database_name "])):
+        if(os.path.exists(data["database_name"])):
+            print("Already Exist")
             return
-        os.mkdir(data[" database_name "])
-        root_path = os.path.realpath(data[" database_name "])
-        for x in data[' Tables ']:
-            path = os.path.join(root_path, x[' name '])
+        os.mkdir(data["database_name"])
+        root_path = os.path.realpath(data["database_name"])
+        for x in data['Tables']:
+            path = os.path.join(root_path, x['name'])
             os.mkdir(path)
          
 
-args = parse_args()
-
-create(args.schema)
+cmd = input("Enter create : ")
+class CommandFactory:
+    args = parse_args()
+    if (cmd == "create") :
+        create(args.schema)
