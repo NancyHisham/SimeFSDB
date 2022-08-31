1d40fa828c3353a62107eb0cfa5161bbfea9adab
MariaBassem123 <96719636+MariaBassem123@users.noreply.github.com>
2022-08-31 20:26:52 +0200

    Update CommandFactory.py

--- CommandFactory.py
+++ CommandFactory.py
@@ -3,8 +3,8 @@ from ParseInput import parse_args
 args= parse_args()
 class CommandFactory:
     @staticmethod
-    def build_command(tt):
-        command_type=tt
+    def build_command(args):
+        command_type=args
         if command_type == "create":
             return CreateCommand().excute()
 #CommandFactory().build_command(args)
