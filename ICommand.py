ee6b9ee56256019a61dde503f9cc2d8b4032963b
MariaBassem123 <96719636+MariaBassem123@users.noreply.github.com>
2022-09-02 21:06:58 +0200

    Delete ICommand.py

--- ICommand.py
+++
@@ -1,6 +0,0 @@
-from abc import ABCMeta, abstractstaticmethod
-# this method is used for creating the ICommand as an abstract class
-class ICommand(metaclass=ABCMeta):
-    @abstractstaticmethod
-    def execute(self):
-        """The interface of ICommand"""
