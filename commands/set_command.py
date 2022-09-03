92f9a449b265526ec33df6cb0f99ffd75ea56700
Hesham-Youssef <hesham_ahmed2002002@yahoo.com>
2022-09-04 01:43:09 +0200

    small edit

--- commands/set_command.py
+++ commands/set_command.py
@@ -8,6 +8,8 @@ def set_command(database, table, primary_key, parameter, value):
     if(not os.path.exists(path)):
         createKey_command.createKey(database, table, primary_key)
     data = json.load(open(path, 'r'))
+    if not (parameter in data):
+        return "Error, key doesn't exist"
     data[parameter] = value
     with open( path , "w") as output:
         output.write(json.dumps(data, indent=4))
