5771e4a430249e114513cdf2c5e6ad69465f2c83
Hesham-Youssef <hesham_ahmed2002002@yahoo.com>
2022-09-04 01:38:12 +0200

    Get, Set and CreateKey method

---
+++ commands/createKey_command.py
@@ -0,0 +1,26 @@
+import json
+import os
+
+
+def createKey(database, table, primary_key):
+    if(not os.path.exists('schema.json')):
+        return
+    schema = json.load(open('schema.json','r'))
+    for item in schema['Tables']:
+        if item['name'] == table:
+            columnsList = item["columns"]
+            break
+    json_columns = {}
+    for parameter in columnsList:
+        json_columns[parameter] = ""
+    path = os.getcwd() + "\ " + database + "\ " + table
+    if(not os.path.exists(path)):
+        return "Error"
+    path = path + "\\" + primary_key + ".json"
+    
+    with open( path , "w") as output:
+        output.write(json.dumps(json_columns, indent=4))
+    
+        
+createKey("Check-in", "FlightInfo", "abc")            
+    
---
+++ commands/set_command.py
@@ -0,0 +1,19 @@
+import json
+import os
+import createKey_command
+
+
+def set_command(database, table, primary_key, parameter, value):
+    path = os.getcwd() + "\ " + database + "\ " + table + "\\" + primary_key + ".json"
+    if(not os.path.exists(path)):
+        createKey_command.createKey(database, table, primary_key)
+    data = json.load(open(path, 'r'))
+    data[parameter] = value
+    with open( path , "w") as output:
+        output.write(json.dumps(data, indent=4))
+    
+    
+
+set_command("Check-in", "FlightInfo", "abc", "PlaneId", "124")
+set_command("Check-in", "FlightInfo", "abc", "AirLineName", "egyptair")
+set_command("Check-in", "FlightInfo", "abc", "From", "alex")
