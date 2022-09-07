0844b7f68c82397a4dfba4cb0b941a68e9cb0ad2
MariaBassem123 <96719636+MariaBassem123@users.noreply.github.com>
2022-09-07 19:02:31 +0200

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from parse_input import parse_args
-from commands.command_factory import CommandFactory
-
-if __name__ == "__main__":
-   args = parse_args()
-   CommandFactory.create_command(args)
-#python main.py -command create -sc schema.json
