50368efdf704ecf107fa43f7aa38a034fa68be4b
MariaBassem123 <96719636+MariaBassem123@users.noreply.github.com>
2022-09-07 19:07:59 +0200

    Delete status_of_code.py

--- status_of_code.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-class StatusOfCode(Enum):
-    SchemaNotFound = "The json file you entered is not found"
-    ErrorLoadingJsonFile = "The data in the json file could not be loaded"
-    DatabaseNameIsMissing = "The database name is missing in the json file"
-    success = "Successfully created a directory"
