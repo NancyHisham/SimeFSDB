171b0c6a5c64687e7f156d24d2e529bc9607fde5
MariaBassem123 <96719636+MariaBassem123@users.noreply.github.com>
2022-09-07 19:03:52 +0200

    Delete command_factory.py

--- commands/command_factory.py
+++
@@ -1,8 +0,0 @@
-from commands.create_command import CreateCommand
-
-class CommandFactory:
-    def create_command(args):
-        command_type = args.command.lower()
-        if (command_type == "create"):
-            return CreateCommand(args.schema)
-        
