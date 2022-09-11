2f2cb6df1a5387f14c71c370039587b0b2ac570a
NancyHisham <96303049+NancyHisham@users.noreply.github.com>
2022-09-11 15:50:32 +0200

    create table and index

--- source/commands/create_command.py
+++ source/commands/create_command.py
@@ -5,15 +5,14 @@ from commands.abstract_command import AbstractCommand
 from commands.keys import *
 from output.exceptions import *
 from model.table import *
-from keys import *
-
-
+from keys import * 
+ 
 
 class CreateCommand(AbstractCommand):
 
     def __init__(self , schema_path):
         self.data = CreateCommand.__validate_and_get_schema(schema_path)  
-        
+
     @staticmethod
     def __validate_and_get_schema(schema_path):
         if (schema_path is None or schema_path == "" or schema_path == " "):
--- source/model/table.py
+++ source/model/table.py
@@ -1,7 +1,7 @@
 from commands.create_command import *
 from commands.keys import *
 from model.index import *
-
+ 
 class Table:
     def serialize(root_path,tablename):
         path = os.path.join(root_path,tablename[keys.NAME]+".json")
