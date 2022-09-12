df498c11a35bb7aa791d506f06ce87d3a10364ec
Hesham-Youssef <hesham_ahmed2002002@yahoo.com>
2022-09-12 21:01:01 +0200

    Revert "removing serialize parameters and adding validate in Tables"
    This reverts commit d9152740aebfc158d08c0fce0287b3c2dc89dca8.

--- source/model/table.py
+++ source/model/table.py
@@ -1,26 +1,10 @@
 from commands.create_command import *
 from commands.keys import *
 from model.index import *
-from output.exceptions import *
-import os
-
-class Table:
-    def __init__(self, root_path, tablename):
-        self.root_path = root_path
-        self.tablename = tablename
-        self.path = os.path.join(self.root_path,self.tablename[keys.NAME])
-        self.validate(self)
-        
-        
-    def serialize(self):
-        path = os.path.join(self.path + ".json")
+ 
+class Table: 
+    def serialize(root_path,tablename):
+        path = os.path.join(root_path,tablename[keys.NAME]+".json")
         with open(path, 'w') as json:
-            Index.index(path,self.tablename)
+            Index.index(path,tablename)
             json.close()
-            
-    @staticmethod
-    def validate(self):
-        if(not os.path.exists(self.root_path)):
-            raise Exception()
-        if(not os.path.exists(self.path)):
-            raise Exception()
