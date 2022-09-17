45d72919efb010aea6ca866a2d30cafee3633e09
Maria Bassem <maria.bassem2002@gmail.com>
2022-09-17 11:08:31 +0200

    Update some points

--- source/model/database.py
+++ source/model/database.py
@@ -4,15 +4,22 @@ from model.table import *
 from output.exceptions import *
 
 class Database:
+    
     def __init__(self):
-        self.schema_data = self.__get_database_schema__()
-        self.__validate(self.schema_data)
-        self.tables = self.__create_tables(self.schema_data[keys.TABLES])
-        
+        schema_data = self.__get_database_schema__()
+        self.__validate(schema_data)
+        self.DB_name = schema_data[keys.DB_NAME]
+        Database.PATH = str(os.getcwd()).replace("model", "") # Path of the source directory
+        self.__path__ = os.path.join(Database.PATH,self.DB_name) # the full path of the database
+        self.tables = self.__create_tables(schema_data[keys.TABLES])
    
-    def __validate(self):
-        if len(self.schema_data[keys.DB_NAME]) == 0 or self.schema_data[keys.DB_NAME].isspace():
-            raise NoParameterError("Database name is missing")
+    @staticmethod
+    def __validate(schema_data):
+        if len(schema_data[keys.DB_NAME]) == 0 or schema_data[keys.DB_NAME].isspace():
+            raise NoParameterError("Database name is not detected")
+
+    def get_path(self):
+        return self.__path__
 
     def create(self):
         self.__create_database()
@@ -20,11 +27,10 @@ class Database:
             table.create()
 
     def __create_database(self):
-        path = os.path.join(str(os.getcwd()).replace("model" , ""), self.schema_data[keys.DB_NAME]) # creates a database in the source folder
-        os.makedirs(path, exist_ok=True)
+        os.makedirs(self.__path__, exist_ok=True)
     
     def __get_database_schema__(self):
-        with open(self.schema_path, 'r') as schema_file:
+        with open(self.__path__, 'r') as schema_file:
             return json.load(schema_file)
     
     def __create_tables(self, schema_tables):
